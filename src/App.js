ab03d0f59b1f76614f3d5e663d4334e1fd89667b
SilverSpear28 <abhijaymessi@gmail.com>
2020-11-30 20:18:22 +0530

    Add react router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home/home'
 import Team from './pages/team/team'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Header from './components/header/header'
+
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Route path='/' exact component={Home}/>
+      <Route path='/about' component={Team}/>
+    </Router>
   );
 }
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,8 +3,14 @@ import Logo from "../../assets/logo.svg";
 import "./header.css";
 import Logo_white from "../../assets/logo-white.svg"
 import Right_arrow from "../../assets/right-arrow.svg"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class Header extends React.Component {
+
+  constructor(props){
+    super(props)
+  }
+
   navActive() {
     var header = document.querySelector(".header");
     var header_reveal = document.querySelector(".header__reveal");
@@ -39,7 +45,7 @@ class Header extends React.Component {
     return (
       <div className="header">
         <div className="header__inner">
-          <img src={Logo} alt="logo" id="logo"/>
+          <a href='/'><img src={Logo} alt="logo" id="logo"/></a>
           <div className="hamburger" onClick={this.navActive.bind(this)}>
             <span></span>
             <span></span>
@@ -49,13 +55,17 @@ class Header extends React.Component {
         <div className="header__reveal">
           <div className="header__reveal__container">
             <div className="header__reveal__container__navlinks">
-              <div className="navlink" id="about"><a href="">About</a></div>
+              <div className="navlink" id="about"><a href='/about'>About</a></div>
               <div className="navlink" id="services"><a href="">Services</a></div>
               <div className="navlink" id="projects"><a href="">Projects</a></div>
               <div className="navlink" id="blogs"><a href="">Blogs</a></div>
               <div className="navlink" id="github"><a href="">Github</a></div>
               <div className="navlink" id="clients"><a href="">Clients</a></div>
             </div>
+            {/* <Switch>
+              <Route exact path='/' component={this.props.index} />
+              <Route path='/team' component={this.props.team} />
+            </Switch> */}
             <div className="header__reveal__container__otherinfo">
               <div className="header__reveal__container__otherinfo__para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis commodo lacus donec facilisi blandit euismod. Semper diam neque volutpat lorem condimentum luctus senectus ullamcorper interdum. Bibendum imperdiet orci bibendum nunc a. In massa lacinia vulputate in tempus. Diam, ullamcorper in enim sed dictum sapien id maecenas. Ut lorem nisl enim, arcu elit mattis convallis mattis nunc. Platea tortor, urna, sed lorem. In ut dolor nunc scelerisque.</div>
               <div className="header__reveal__container__otherinfo__link"><a href="">Goto Forest <img src={Right_arrow} alt=""/></a></div>
