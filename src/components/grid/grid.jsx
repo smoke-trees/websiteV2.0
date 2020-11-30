577fcaba869a4d1614801e608092bb561c82f018
SilverSpear28 <abhijaymessi@gmail.com>
2020-11-30 19:20:45 +0530

    Masonry layout added in grid

--- src/components/grid/grid.jsx
+++ src/components/grid/grid.jsx
@@ -4,19 +4,30 @@ import './grid.css'
 import Img from '../../assets/testimg.svg'
 import Img1 from '../../assets/img1.svg'
 import Img2 from '../../assets/im3.svg'
+import Masonry from 'react-masonry-css'
 
 class Grid extends React.Component{
   constructor(props){
     super(props)
+
+    this.state = {
+      breakpointColsObj: {
+        default: 2,
+        600: 1
+      }
+    }
   }
   render(){
     return(
-      <div className='grid'>
-        <Card img={Img}/>
-        <Card img={Img1}/>
-        <Card img={Img2}/>
-        <Card img={Img}/>
-      </div>
+      <Masonry
+      breakpointCols={this.state.breakpointColsObj}
+      className='my-masonry-grid mobile'
+      columnClassName='my-masonry-grid_column'
+      >
+        <Card img={Img} />
+        <Card img={Img1} />
+        <Card img={Img2} />
+      </Masonry>
     )
   }
 }
