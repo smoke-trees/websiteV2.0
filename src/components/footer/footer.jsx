87614b3d624b025e0f359e1ffdfcf5b265086372
Anshuman Chhapolia <anshuman@smoketrees.in>
2021-07-06 13:41:52 +0530

    feat: add links to footers
    Signed-off-by: Anshuman Chhapolia <anshuman@smoketrees.in>

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,41 +5,47 @@ import icon2 from '../../assets/linkedin.svg'
 import icon3 from '../../assets/github1.svg'
 import iconright from '../../assets/right-arrow.svg'
 
-class Footer extends React.Component{
-  constructor(props){
+class Footer extends React.Component {
+  constructor(props) {
     super(props)
   }
-  render(){
-    return(
+  render() {
+    return (
       <div className='footer'>
         <div className='footer__details'>
           <div className='footer__details__list'>
             <div className='footer__details__list__one'>
               <ul>
-                <li>About</li>
-                <li>Services</li>
-                <li>Projects</li>
-                <li>Clients</li>
+                <li><a href="/about">About</a></li>
+                <li><a href='/clients'>Clients</a></li>
+                <li><a href='/services'>Services</a></li>
+                <li><a href='/projects'>Projects</a></li>
               </ul>
             </div>
             <div className='footer__details__list__one'>
               <ul>
-                <li style={{textAlign: 'right'}}>Contact Us <br></br> info@smoketrees.in</li>
+                <li style={{ textAlign: 'right' }}>Contact Us <br></br>
+                  <a
+                    href='mailto:info@smoketrees.in'
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >info@smoketrees.in</a>
+                </li>
               </ul>
             </div>
           </div>
           <div className='footer__social'>
             <div className='footer__social__icons'>
               <a href="https://www.instagram.com/smoketrees_digital/" rel='noopener noreferrer' target='_blank'>
-              <img src={icon1} alt="Instagram" className="footer__social__icons--style" />
+                <img src={icon1} alt="Instagram" className="footer__social__icons--style" />
               </a>
               <a href="https://www.linkedin.com/company/smoke-trees/" className="footer__social__icons--style" rel='noopener noreferrer' target='_blank'>
-              <img src={icon2} alt="Linkedin" className="footer__social__icons--style" />
+                <img src={icon2} alt="Linkedin" className="footer__social__icons--style" />
               </a>
               <a href="https://github.com/smoke-trees" className="footer__social__icons--style footer__social__github" rel='noopener noreferrer' target='_blank'>
-              <img src={icon3} alt="Github" className="footer__social__icons--style" />
+                <img src={icon3} alt="Github" className="footer__social__icons--style" />
               </a>
-              </div>
+            </div>
             <div className='footer__social__copy'>
               SmokeTrees Digital LLP
             </div>
